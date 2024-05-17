02c0793b8b498601ce562a81f2b933ca857270fb
wicifer <wictorfts@gmail.com>
2024-05-17 18:12:00 -0300

    englobando em num metodo principal

--- funcionalidades/recomendacoes.py
+++ funcionalidades/recomendacoes.py
@@ -1,4 +1,6 @@
 # Dicionário de livros com suas categorias
+
+# TO DO: colocar num arquivo
 livros = {
     "A Culpa é das Estrelas": ["Romance", "Drama"],
     "1984": ["Ficção Científica", "Distopia"],
@@ -21,38 +23,39 @@ def recomendar_livros_por_categoria(categoria):
     return recomendacoes
 
 # Função principal
-print("Bem-vindo ao sistema de recomendação de livros!")
-print("Escolha uma categoria para receber recomendações:")
-print("1. Romance")
-print("2. Ficção Científica")
-print("3. Fantasia")
-print("4. Suspense")
-print("5. Clássico")
-print("6. Outro")
+def main():
+    print("Bem-vindo ao sistema de recomendação de livros!")
+    print("Escolha uma categoria para receber recomendações:")
+    print("1. Romance")
+    print("2. Ficção Científica")
+    print("3. Fantasia")
+    print("4. Suspense")
+    print("5. Clássico")
+    print("6. Outro")
 
-escolha = input("\nDigite o número correspondente à categoria desejada: ")
+    escolha = input("\nDigite o número correspondente à categoria desejada: ")
+# TO DO: opção inválida
+    if escolha == "1":
+        categoria = "Romance"
+    elif escolha == "2":
+        categoria = "Ficção Científica"
+    elif escolha == "3":
+        categoria = "Fantasia"
+    elif escolha == "4":
+        categoria = "Suspense"
+    elif escolha == "5":
+        categoria = "Clássico"
+    else:
+        categoria = input("Digite outra categoria: ")
 
-if escolha == "1":
-    categoria = "Romance"
-elif escolha == "2":
-    categoria = "Ficção Científica"
-elif escolha == "3":
-    categoria = "Fantasia"
-elif escolha == "4":
-    categoria = "Suspense"
-elif escolha == "5":
-    categoria = "Clássico"
-else:
-    categoria = input("Digite outra categoria: ")
+    recomendacoes = recomendar_livros_por_categoria(categoria)
 
-recomendacoes = recomendar_livros_por_categoria(categoria)
-
-if recomendacoes:
-    print(f"\nRecomendações na categoria '{categoria}':")
-    for livro in recomendacoes:
-        print("-", livro)
-    print("\n")    
-else:
-    print("Não foram encontradas recomendações para essa categoria.")
+    if recomendacoes:
+        print(f"\nRecomendações na categoria '{categoria}':")
+        for livro in recomendacoes:
+            print("-", livro)
+        print("\n")    
+    else:
+        print("Não foram encontradas recomendações para essa categoria.")
 
 
